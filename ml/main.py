c8827c3bc7bc9ca24bd534097819ea5dfc98f604
AndreyKondakovGW <andreykondakov2302@gmail.com>
2021-12-11 18:26:29 +0300

    Создал МЛ Модель

--- ml/main.py
+++ ml/main.py
@@ -1,10 +1,36 @@
-# This is a sample Python script.
 import sys
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from recognition_model import Recognition_model1
+from emotion_recognition.emotion_model import Emotion_model1
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import os
+import json
+import operator
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print(f"{sys.argv[1]}_____{sys.argv[2]}");
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@hydra.main(config_path="conf", config_name="config")
+def app(cfg : DictConfig) -> None:
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    model = Recognition_model1(**cfg.params)
+    emo_model = Emotion_model1(**cfg.emo_params)
+    input_dir = dir_name + cfg.input_folder
+
+
+    template_path = sys.argv[1]
+    samples_path = sys.argv[2]
+    output_path = dir_name + cfg.output_folder
+
+
+    prediction = model.gen_simularity_by_template(template_path, samples_path)
+    emo_pred = emo_model.predict_from_folder(samples_path)
+    if (len(emo_pred) == 0):
+        max_emo = 'neutral'
+    else:
+        max_emo = max(emo_pred.items(), key=operator.itemgetter(1))[0]
+    prediction["emo"] = max_emo
+    with open(output_path + '/result.json', 'w') as f:
+        json.dump(prediction, f)
+if __name__ == "__main__": 
+    app()
+
+
---
+++ ml/run.py
@@ -0,0 +1,31 @@
+from recognition_model import Recognition_model1
+from emotion_recognition.emotion_model import Emotion_model1
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import os
+import json
+import operator
+@hydra.main(config_path="conf", config_name="config")
+def app(cfg : DictConfig) -> None:
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    model = Recognition_model1(**cfg.params)
+    emo_model = Emotion_model1(**cfg.emo_params)
+    input_dir = dir_name + cfg.input_folder
+
+
+    template_path = input_dir + cfg.template_path
+    samples_path = input_dir + cfg.samples_folder
+    output_path = dir_name + cfg.output_folder
+
+
+    prediction = model.gen_simularity_by_template(template_path, samples_path)
+    emo_pred = emo_model.predict_from_folder(samples_path)
+    if (len(emo_pred) == 0):
+        max_emo = 'neutral'
+    else:
+        max_emo = max(emo_pred.items(), key=operator.itemgetter(1))[0]
+    prediction["emo"] = max_emo
+    with open(output_path + '/result.json', 'w') as f:
+        json.dump(prediction, f)
+if __name__ == "__main__": 
+    app()
